7ec6f8c29f72e274a80b237bd98b1c231f10b9b6
Md. Jahid Hasan <itsjahidhasan@outlook.com>
2024-05-28 02:22:24 +0600

    single user get and getAll controller added

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -60,4 +60,26 @@ const loginUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser, loginUser };
+const findUser = async (req, res) => {
+  const userId = req.params.userId;
+  try {
+    const user = await userModel.findById(userId);
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.log({ err });
+    res.status(500).json({ err });
+  }
+};
+const getUsers = async (req, res) => {
+  try {
+    const users = await userModel.find();
+
+    res.status(200).json(users);
+  } catch (err) {
+    console.log({ err });
+    res.status(500).json({ err });
+  }
+};
+
+module.exports = { registerUser, loginUser, findUser, getUsers };
