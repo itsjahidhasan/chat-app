d6e35649f05c71a9fc29ca76a8eedc2b2c3bfe1f
md.hasan <md.hasan@technonext.com>
2024-05-27 19:24:07 +0600

    route added

--- server/index.js
+++ server/index.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoute = require("./Routes/userRoute");
 
 const app = express();
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
+app.use("/api/users", userRoute);
+
+app.get("/api", (req, res) => {
+  res.send("api");
+});
 
 const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
 
 app.listen(port, (req, res) => {
   console.log(`listening on port... ---: ${port}`);
 });
+
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connection established");
+  })
+  .catch((err) => console.log("Connection error: ", err.message));
