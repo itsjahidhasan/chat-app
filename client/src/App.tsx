d85a561a84a2070a49284bd22806108730905e2a
Md. Jahid Hasan <itsjahidhasan@outlook.com>
2024-05-31 02:34:01 +0600

    react router dom and navbar added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,25 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Chat from "./pages/chat";
+import Register from "./pages/register";
+import Login from "./pages/login";
+import "bootstrap/dist/css/bootstrap.css";
+import { Container } from "react-bootstrap";
+import NavBar from "./components/layout/shared/navbar/nav-bar";
+
 function App() {
-  return <>Chat App</>;
+  return (
+    <>
+      <NavBar />
+      <Container className="text-secondary">
+        <Routes>
+          <Route path="/" element={<Chat />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to={"/"} />} />
+        </Routes>
+      </Container>
+    </>
+  );
 }
 
 export default App;
