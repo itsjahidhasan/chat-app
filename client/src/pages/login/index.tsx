cd63c2c5522f6f1911cdbf28cee13fff50c59973
Md. Jahid Hasan <itsjahidhasan@outlook.com>
2024-05-31 02:49:32 +0600

    Login and register form created

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,5 +1,34 @@
+import { Alert, Button, Col, Form, Row, Stack } from "react-bootstrap";
+
 const Login = () => {
-  return <>Login page</>;
+  return (
+    <>
+      <Form>
+        <Row
+          style={{
+            height: "100vh",
+            justifyContent: "center",
+            paddingTop: "10%",
+          }}
+        >
+          <Col xs={6}>
+            <Stack gap={3}>
+              <h2>Login</h2>
+              <Form.Control type="text" placeholder="Name"></Form.Control>
+              <Form.Control type="text" placeholder="Name"></Form.Control>
+              <Button variant="primary" type="submit">
+                Login
+              </Button>
+
+              <Alert variant="danger">
+                <p>An error occured</p>
+              </Alert>
+            </Stack>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
 };
 
 export default Login;
--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -1,5 +1,35 @@
+import { Alert, Button, Col, Form, Row, Stack } from "react-bootstrap";
+
 const Register = () => {
-  return <>Register page</>;
+  return (
+    <>
+      <Form>
+        <Row
+          style={{
+            height: "100vh",
+            justifyContent: "center",
+            paddingTop: "10%",
+          }}
+        >
+          <Col xs={6}>
+            <Stack gap={3}>
+              <h2>Register</h2>
+              <Form.Control type="text" placeholder="Name"></Form.Control>
+              <Form.Control type="text" placeholder="Name"></Form.Control>
+              <Form.Control type="text" placeholder="Name"></Form.Control>
+              <Button variant="primary" type="submit">
+                Register
+              </Button>
+
+              <Alert variant="danger">
+                <p>An error occured</p>
+              </Alert>
+            </Stack>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
 };
 
 export default Register;
